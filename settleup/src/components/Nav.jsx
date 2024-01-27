ac01da8096d7a6d22dc49a6749cbe2a70b1ad7fc
vaishnavi rathore <vaishnavirathore690@gmail.com>
2024-01-27 23:39:00 +0530

    ADDED CARDS

--- settleup/src/components/Nav.jsx
+++ settleup/src/components/Nav.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import './Nav.css';
-
+import React from "react";
+import "./Nav.css";
 
 function Nav() {
   return (
@@ -9,43 +8,96 @@ function Nav() {
         <img className="logo" src="/images/logo.png" alt="Logo" />
         <nav className="navbar">
           <ul className="navbar-list">
-            <li><a href="#" className="navbar-link">Home</a></li>
-            <li><a href="#" className="navbar-link">Profile</a></li>
-            <li><a href="#" className="navbar-link">Services</a></li>
-            <li><a href="#" className="navbar-link" target="_blank">About</a></li>
+            <li>
+              <a href="#" className="navbar-link">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="navbar-link">
+                Profile
+              </a>
+            </li>
+            <li>
+              <a href="#" className="navbar-link">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#" className="navbar-link" target="_blank">
+                About
+              </a>
+            </li>
           </ul>
         </nav>
       </header>
-     <main>
-      <section className="section-hero">
-
-       
-        <div className="head">
-        <h1>Say goodbye to bill payment stress,<br/> and hello to convenience.</h1>
-        <h2>
-         <br/>
-          The ultimate solution for splitting bills with friends.<br/>
-          Settle your bills with us, and enjoy peace of mind.
-          </h2>
-          <button className="btn">Add Members</button>
-        </div>
+      <main>
+        <section className="section-hero">
+          <div className="head">
+            <h1>
+              Say goodbye to bill payment stress,
+              <br /> and hello to convenience.
+            </h1>
+            <h2>
+              <br />
+              The ultimate solution for splitting bills with friends.
+              <br />
+              Settle your bills with us, and enjoy peace of mind.
+            </h2>
+            <button className="btn">Add Members</button>
+          </div>
           <div className="hero-img">
-          <img src="/images/6381.jpg" />
+            <img src="/images/6381.jpg" />
+          </div>
+        </section>
+      </main>
+      <section className="section-services">
+        <div className="sec">
+          <div className="sec_1" style={{ backgroundColor: "#cff4f7" }}>
+            <p>
+              SplitEase allows you to split bills equally among participants
+              with just a few taps.
+            </p>
+          </div>
+          <div className="sec_2" style={{ backgroundColor: "rgba(45, 45, 45, .4) " }}>
+            <p>
+              SplitEase allows you to split bills equally among participants
+              with just a few taps.
+            </p>
+          </div>
+          <div className="sec_2" style={{ backgroundColor: "rgba(45, 45, 45, .4) " }}>
+            <p>
+              SplitEase allows you to split bills equally among participants
+              with just a few taps.
+            </p>
+          </div>
         </div>
-       
-       
+        <div className="sec">
+          <div className="sec_2" >
+            <p>
+              SplitEase allows you to split bills equally among participants
+              with just a few taps.
+            </p>
+          </div>
+          <div className="sec_2" style={{ backgroundColor: "rgba(45, 45, 45, .4) " }}>
+            <p>
+              SplitEase allows you to split bills equally among participants
+              with just a few taps.
+            </p>
+          </div>
+          <div className="sec_2" style={{ backgroundColor: "rgba(45, 45, 45, .4) " }}>
+            <p>
+              SplitEase allows you to split bills equally among participants
+              with just a few taps.
+            </p>
+          </div>
+        </div>
       </section>
-     </main>
-       <section className="section-services">
-        <div className="hero">
-          <h2>Our Services:</h2>
-        </div>
-       </section>
-       <section className="section-add">
+      <section className="section-add">
         <div className="hero">
           <h2>Add members</h2>
         </div>
-       </section>
+      </section>
     </>
   );
 }
